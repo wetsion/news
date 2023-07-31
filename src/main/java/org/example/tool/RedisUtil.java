53b2378394ba3ce86bbadf686421b9517cc806b9
霜华 <shuanghua@fordeal.com>
2023-08-01 01:23:09 +0800

    add

--- src/main/java/org/example/tool/RedisUtil.java
+++ src/main/java/org/example/tool/RedisUtil.java
@@ -23,7 +23,7 @@ public class RedisUtil {
             return;
         }
         config = new Config();
-        config.useSingleServer().setAddress("redis://10.200.0.7:6379")
+        config.useSingleServer().setAddress("redis://10.203.0.15:6379")
                 .setPassword("weixin").setDatabase(1);
 
         redissonClient = Redisson.create(config);
